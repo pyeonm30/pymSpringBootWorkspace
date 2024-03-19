890351b0d9f772a579d7ccc486243e4b8d6b5f13
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-03-19 15:19:40 +0900

    .
    .

--- jpa/src/main/java/JpaMain.java
+++ jpa/src/main/java/JpaMain.java
@@ -1,11 +1,11 @@
 import entity.Customer;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JpaMain {
+
     public static void main(String[] args) {
         // 객체 sessitonFactory
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("customer-ex");
@@ -14,22 +14,11 @@ public class JpaMain {
 
         tx.begin(); // start transction;
         try {
+            Customer c = new Customer("test");
 
-//            Customer c1 = new Customer("ID007", "Lee3");
-//            Customer c2 = new Customer("ID008", "Hong4");
-//            em.persist(c1); // 영속성 컨텍스트에 객체를 저장 insert
-//            em.persist(c2); // insert
-            Customer c1 = em.find(Customer.class,"ID001");
-            em.remove(c1); // sql 쓰기지연에다가 쿼리문 저장 
-            System.out.println("c1 = " + c1);
+            em.persist(c);
 
-            Customer findCustomer1 = em.find(Customer.class, "ID007"); // 이미 영속성 객체 안에 있으면 select 쿼리문 안나간다
-
-            Customer findCustomer2 = em.find(Customer.class, "ID005"); // 영속성 컨텍스트에 객체가 없을때만 select 쿼리문이 나간다
-            System.out.println("findCustomer1 = " + findCustomer1);
-            System.out.println("findCustomer2 = " + findCustomer2);
-
-            tx.commit(); // commit; 쓰기지연 저장소에있는 sql 쿼리문(insert, update, delete ) 한꺼번에 나간다
+            tx.commit();
 
         }catch(Exception e){
             tx.rollback();
--- jpa/src/main/java/entity/Customer.java
+++ jpa/src/main/java/entity/Customer.java
@@ -4,23 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
 @Entity
 //@NoArgsConstructor // 기본생성자 반드시 필수!!  JPA -> new Customer()..
+@Table(name="Customer_tb")
 public class Customer {
     @Id
-    String id;
+    @Column(name="cutomer_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
     String name;
     LocalDate regDate;
 
     public Customer(){}
 
-    public Customer(String id, String name) {
-        this.id = id;
+    public Customer(String name) {
         this.name = name;
         regDate = LocalDate.now();
     }
