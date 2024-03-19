2e02cfda665a3529438b70e42dd3b7b33fa2e384
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-03-19 16:01:39 +0900

    /
    /

--- jpa/src/main/java/entity/Major.java
+++ jpa/src/main/java/entity/Major.java
@@ -1,22 +1,25 @@
 package entity;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Getter
-@Setter
+@Data
 @Entity
 @NoArgsConstructor
+@Table(name="major_tbl")
 public class Major {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long majorid;
+    private Long majorId;
     private String name;
     private String category;
+
+    public Major(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
 }
--- jpa/src/main/java/entity/Student.java
+++ jpa/src/main/java/entity/Student.java
@@ -1,23 +1,26 @@
 package entity;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Getter
-@Setter
+@Data
 @Entity
 @NoArgsConstructor
+@Table(name="student_tbl")
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long stuentid;
+    private Long stuentId;
     private String name;
     private String grade;
+    private Long majorId;
 
+    public Student(String name, String grade) {
+        this.name = name;
+        this.grade = grade;
+    }
 }
