d0e628bf89d01214b96f6062cfc4b72c2468c5e8
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-03-19 12:46:56 +0900

    .
    .

--- jpa/src/main/java/JpaMain.java
+++ jpa/src/main/java/JpaMain.java
@@ -12,10 +12,31 @@ public class JpaMain {
         EntityManager em = emf.createEntityManager(); // session 객체
         EntityTransaction tx = em.getTransaction();
 
-        tx.begin();
-        Customer c = new Customer("ID001" , "KIM");
-        em.persist(c);
-        tx.commit();
+        tx.begin(); // start transction;
+        try {
+
+//            Customer c1 = new Customer("ID007", "Lee3");
+//            Customer c2 = new Customer("ID008", "Hong4");
+//            em.persist(c1); // 영속성 컨텍스트에 객체를 저장 insert
+//            em.persist(c2); // insert
+            Customer c1 = em.find(Customer.class,"ID001");
+            em.remove(c1); // sql 쓰기지연에다가 쿼리문 저장 
+            System.out.println("c1 = " + c1);
+
+            Customer findCustomer1 = em.find(Customer.class, "ID007"); // 이미 영속성 객체 안에 있으면 select 쿼리문 안나간다
+
+            Customer findCustomer2 = em.find(Customer.class, "ID005"); // 영속성 컨텍스트에 객체가 없을때만 select 쿼리문이 나간다
+            System.out.println("findCustomer1 = " + findCustomer1);
+            System.out.println("findCustomer2 = " + findCustomer2);
+
+            tx.commit(); // commit; 쓰기지연 저장소에있는 sql 쿼리문(insert, update, delete ) 한꺼번에 나간다
+
+        }catch(Exception e){
+            tx.rollback();
+        }finally{
+            em.close();
+            emf.close();
+        }
 
     }
 }
--- jpa/src/main/java/entity/Customer.java
+++ jpa/src/main/java/entity/Customer.java
@@ -1,5 +1,6 @@
 package entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,16 +10,24 @@ import java.time.LocalDate;
 
 @Data
 @Entity
-@NoArgsConstructor // 기본생성자 반드시 필수!!
+//@NoArgsConstructor // 기본생성자 반드시 필수!!  JPA -> new Customer()..
 public class Customer {
     @Id
     String id;
     String name;
     LocalDate regDate;
 
+    public Customer(){}
+
     public Customer(String id, String name) {
         this.id = id;
         this.name = name;
         regDate = LocalDate.now();
     }
+
+//    public Customer(String id, String name, LocalDate regDate) {
+//        this.id = id;
+//        this.name = name;
+//        this.regDate = regDate;
+//    }
 }
