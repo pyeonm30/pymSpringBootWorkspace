8f71d264036bc9019ce352c1bd5c432bfb693d5e
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-03-20 18:42:47 +0900

    Create StudyRecodeRepositroy.java
    .

---
+++ jpa1/src/main/java/kr/study/jpa1/repository/StudyRecodeRepositroy.java
@@ -0,0 +1,7 @@
+package kr.study.jpa1.repository;
+
+import kr.study.jpa1.domain.StudyRecode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyRecodeRepositroy extends JpaRepository<StudyRecode, Long> {
+}
