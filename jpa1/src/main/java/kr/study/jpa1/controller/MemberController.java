3ac73acdb47a69748ddd65488191018a5cad97ba
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-03-20 14:01:52 +0900

    .
    .

---
+++ jpa1/src/main/java/kr/study/jpa1/controller/MemberController.java
@@ -0,0 +1,10 @@
+package kr.study.jpa1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+}
