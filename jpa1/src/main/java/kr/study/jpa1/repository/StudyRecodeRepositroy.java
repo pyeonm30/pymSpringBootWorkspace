33b4fecb3d058d3fee36e78619ffc35f044f56b8
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-03-21 15:58:53 +0900

    .
    .

--- jpa1/src/main/java/kr/study/jpa1/repository/StudyRecodeRepositroy.java
+++ jpa1/src/main/java/kr/study/jpa1/repository/StudyRecodeRepositroy.java
@@ -12,7 +12,7 @@ public interface StudyRecodeRepositroy extends JpaRepository<StudyRecode, Long> 
     @Query(value="select m.member_id, r.study_id, r.contents, r.start_time, r.study_day, r.study_mins from member m , study_recode r where m.member_id = r.member_id", nativeQuery = true)
     List<StudyRecode> selectAll();
 
-    @Query(value="select * from study_recode where member_id = :memberId", nativeQuery = true)
+    @Query(value="select * from study_recode where member_id =:memberId", nativeQuery = true)
     List<StudyRecode> searchStudyRecodeByMemberId(@Param(value="memberId") Long memberId);
 
     //delete study_recode where member_id = memberId;
