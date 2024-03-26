8dd497cd5cbb812446becf36d90d049b53ab3c5b
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-03-26 17:56:48 +0900

    Update StockServiceTest.java
    .

--- stock/src/test/java/com/basic/stock/service/StockServiceTest.java
+++ stock/src/test/java/com/basic/stock/service/StockServiceTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -31,6 +35,38 @@ class StockServiceTest {
         System.out.println("stock = " + stock);
         System.out.println("count = " + stock.getQuantity());
         Assertions.assertThat(stock.getQuantity()).isEqualTo(99);
+
+    }
+
+    @Test
+    public void orderSametime100Stock() throws InterruptedException {
+
+        int treadCount = 100; // 쓰레드를 100개 설정
+        // 비동기를 편리하게 도와주는 클래스 : 동시에 32개 쓰레드 비동기로 수행가능
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        // 100개의 요청이 모두 끝날때까지 기다려야함으로 CounterDownLatch
+        // 다른 쓰레드에서 수행되는 작업이 완료될때까지 대기할 수 있도록 도와주는 클래스
+        CountDownLatch latch = new CountDownLatch(treadCount);
+
+        for(int i =0; i < treadCount; i++){
+
+            executorService.submit(()->{
+                try{
+                    service.decreaseStock(1L,1L);
+                }catch (Exception e){
+
+                }finally{
+                    latch.countDown();
+                }
+            });
+
+        }
+
+        latch.await();
+        Stock stock = repository.findById(1L).orElseThrow();
+        System.out.println("stock = " + stock);
+        Assertions.assertThat(stock.getQuantity()).isEqualTo(0);
 
     }
 
