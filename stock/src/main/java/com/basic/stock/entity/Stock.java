9fa3136869461c1d06973d14f79155907440cd23
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-03-26 17:10:40 +0900

    .
    .

---
+++ stock/src/main/java/com/basic/stock/entity/Stock.java
@@ -0,0 +1,34 @@
+package com.basic.stock.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId; // 상품 id
+    private Long quantity; // 수량
+
+    public Long getQuantity(){
+        return quantity;
+    }
+
+    // 재고 감소 로직
+    public void decreaseStock(Long quantity){
+        if(this.quantity - quantity < 0){
+            throw new RuntimeException("재고 수량은 0보다 작을 수 없습니다");
+        }
+        this.quantity -= quantity;
+    }
+
+
+}
